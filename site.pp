847220fa18aa405e769667ddce794d98e3858981
avkachan <kachan.andrew.v@gmail.com>
2016-09-22 16:35:48 +0300

    lesson13

---
+++ site.pp
@@ -0,0 +1,33 @@
+class { '::mysql::server':
+    root_password    => 'Epam_2011',
+    override_options => {
+      'mysqld' => { 'max_connections' => '1024' }
+    },
+  }
+  mysql_database { 'prod_mdb':
+      ensure  => present,
+      charset => 'utf8',
+  }
+  mysql_user { 'prod_user@localhost':
+    ensure => present,
+  }
+  mysql_grant { 'prod_user@localhost/prod_mdb.*':
+    ensure      => present,
+    options     => ['GRANT'],
+    privileges  => ['ALL'],
+    table       => 'prod_mdb.*',
+    user        => 'prod_user@localhost',
+  }
+  package { 'nginx':
+        ensure=>'installed'
+  }
+
+  notify { 'Nginx is installed.':
+  }
+
+  service { 'nginx':
+        ensure=>'running'
+  }
+
+  notify { 'Nginx is running.':
+  }
